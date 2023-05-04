c50a9353f025bc46cb3fad087b1d4e9d355fd4a0
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-05-04 12:29:29 +0200

    adding Discover & Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+
+const List = ({ items, mt }: { items: string[]; mt: boolean }) => (
+  <div className={`${mt && "mt-5"} flex flex-wrap gap-2`}>
+    {items.map((item, index) => (
+      <p
+        key={index}
+        className="text-gray-400 text-sm hover:undeline cursor-pointer"
+      >
+        {item}
+      </p>
+    ))}
+  </div>
+);
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} mt={false} />
+      <List items={footerList2} mt />
+      <List items={footerList3} mt />
+      <p className="text-gray-400 text-sm mt-5">2023 TikTik</p>
+    </div>
+  );
 };
 
 export default Footer;
