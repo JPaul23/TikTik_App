1efbfa7501c005ac02b2d458ff0ef731a469a1e5
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-05-04 16:55:55 +0200

    Added Video view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import axios from "axios";
 
-const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
+interface IProps {
+  videos: Video[];
 }
 
-export default Home
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text="No Videos" />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+  console.log(data);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+export default Home;
